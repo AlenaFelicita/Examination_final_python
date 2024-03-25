29d572024ad02fa3911ab904bb5e4100cf8f55d2
AlenaFelicita <118136714+AlenaFelicita@users.noreply.github.com>
2024-03-25 14:12:57 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -35,4 +35,4 @@ from check_site_linux import site_is_not_vulnerable
 
 
 def test_site_vulnerability(site):
-    assert site_is_not_vulnerable(site), f'Site {site} is vulnerable'
+    assert site_is_not_vulnerable(site), f'Site {site} is vulnerable' 
