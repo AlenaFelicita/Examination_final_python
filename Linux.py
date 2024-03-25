2035531514bd5f918986f998a99bf106c31f7057
AlenaFelicita <118136714+AlenaFelicita@users.noreply.github.com>
2024-03-25 14:12:14 +0300

    Update Linux.py

--- Linux.py
+++ Linux.py
@@ -4,4 +4,4 @@ import subprocess
 def site_is_not_vulnerable(site):
     result = subprocess.run(f'nikto -h {site} -ssl -Tuning 4', shell=True,
                             stdout=subprocess.PIPE, encoding='utf-8')
-    return result.returncode == 0 and '0 error(s)' in result.stdout
+    return result.returncode == 0 and '0 error(s)' in result.stdout 
